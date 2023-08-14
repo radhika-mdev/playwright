c30eedf697a8e87621c621730f508509b3fc53ab
radz <radhikam86@gmail.com>
2023-08-14 19:25:03 +0530

    list related exercise

---
+++ pythonProject/list.py
@@ -0,0 +1,24 @@
+#exercise1
+l1=[1,2]
+l2=[5,6]
+l3=l1+l2
+print(l3)
+
+#exercise2
+squares = [1, 4, 9, 16]
+sum=0
+for i in squares:
+    sum+=i
+print(sum)
+
+#exercise3
+list1 = ['larry', 'curly', 'moe']
+if 'curluy' in list1:
+    print("yay")
+else:
+    print("Nah")
+
+#exercise4
+st="Radhika"
+for i in st:
+    print(i+"s")
